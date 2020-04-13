63c881170a43443b82be517c25636ad92920fe13
Pokour <pokour@gmail.com>
2020-04-13 11:20:59 +0530

    get Expres

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-// var express = require('express');
-// var greet = require('./customModule');
+/*************************************
+ * imports and declarations
+ */
+const express = require('express');
+const app = express();
+app.use(express.json());
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'}
+];
 
-// var app = express();
-
-// app.use(express.static(__dirname));
-
-// greet.greetings();
-
-// app.listen(3000);
-
-const http = require('http');
-
-const server = http.createServer( (req, res) => {
-    console.log(req.url, req.method, req.headers);
+/**************************************
+ * SERVER CODE
+ */
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-server.listen(3000);  
+app.get('/api/courses', (req, res) =>{
+    res.send(courses);
+});
+
+app.get('/api/posts/:year/:month', (req,res) =>{
+    res.send(req.params.year);
+});
+
+app.get('/api/courses/:id', (req,res) =>{
+    const course = courses.find( (c) => {
+        if(c.id === parseInt(req.params.id)){
+            return c
+        }});
+    if(!course) {res.status(404).send('The course with given id was not found')}
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) =>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.listen(3000, () => {
+    console.log('The server is listening on PORT 3000');
+});
